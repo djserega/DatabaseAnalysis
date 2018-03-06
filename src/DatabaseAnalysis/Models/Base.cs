13731e3c8c6b8b62485f1078b00f943f103ee185
dj_serega <galyuk.sergey@gmail.com>
2018-03-07 00:16:31 +0200

    filling an object through reflection

--- src/DatabaseAnalysis/Models/Base.cs
+++ src/DatabaseAnalysis/Models/Base.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace DatabaseAnalysis.Models
 {
@@ -36,5 +37,112 @@ namespace DatabaseAnalysis.Models
         public string Password { get; set; }
 
         public ICollection<BaseStructures> Structures { get; set; }
+
+        public Base()
+        {
+            Structures = new List<BaseStructures>();
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                switch (propertyName)
+                {
+                    case "Name":
+                        return Name;
+                    case "Comment":
+                        return Comment;
+                    case "Server":
+                        return Server;
+                    case "BaseName":
+                        return BaseName;
+                    case "UserDB":
+                        return UserDB;
+                    case "PasswordDB":
+                        return PasswordDB;
+                    case "URI":
+                        return URI;
+                    case "User":
+                        return User;
+                    case "Password":
+                        return Password;
+                    default:
+                        throw new NotImplementedException($"Получение свойства {propertyName} не реализовано.");
+                }
+            }
+            set
+            {
+                switch (propertyName)
+                {
+                    case "Name":
+                        Name = value;
+                        break;
+                    case "Comment":
+                        Comment = value;
+                        break;
+                    case "Server":
+                        Server = value;
+                        break;
+                    case "BaseName":
+                        BaseName = value;
+                        break;
+                    case "UserDB":
+                        UserDB = value;
+                        break;
+                    case "PasswordDB":
+                        PasswordDB = value;
+                        break;
+                    case "URI":
+                        URI = value;
+                        break;
+                    case "User":
+                        User = value;
+                        break;
+                    case "Password":
+                        Password = value;
+                        break;
+                    default:
+                        throw new NotImplementedException($"Установка свойства {propertyName} не реализовано.");
+                }
+            }
+        }
+
+
+        internal void StructureDBToStructures(List<StructureDB> list)
+        {
+            Structures.Clear();
+
+            PropertyInfo[] propertysStructureDB = typeof(BaseStructures).GetProperties();
+            PropertyInfo[] propertysBaseStructureDB = typeof(BaseStructureDB).GetProperties();
+
+            foreach (StructureDB itemStructureDB in list)
+            {
+                BaseStructures baseStructures = new BaseStructures
+                {
+                    Base = this,
+                    Date = DateTime.Now
+                };
+
+                BaseStructureDB baseStructureDB = new BaseStructureDB();
+
+                foreach (PropertyInfo propInfo in propertysStructureDB)
+                {
+                    PropertyInfo propInfoBase = propertysBaseStructureDB.FirstOrDefault(f => f.Name == propInfo.Name);
+
+                    if (propInfoBase != null)
+                    {
+                        if (propInfo.PropertyType == typeof(string))
+                        {
+                            baseStructureDB[propInfo.Name] = itemStructureDB[propInfo.Name];
+                        }
+                    }
+                }
+
+
+                Structures.Add(baseStructures);
+            }
+        }
+
     }
 }
